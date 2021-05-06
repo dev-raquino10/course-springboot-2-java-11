44f5a04f773e5f02e94b8fecbf3d81201a45a043
Rafael Alexandre de Aquino <dev.raquino@gmail.com>
2021-05-06 19:56:32 -0300

    User entity and resource

---
+++ src/main/java/com/raquino/course/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.raquino.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.raquino.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Rafael", "rafa.aquino@gmail.com", "11975503833", "101010");
+		return ResponseEntity.ok().body(u);
+	}
+}
